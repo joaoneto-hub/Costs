3f817d87b9a202756cc22e153a9ee373a2bbb8d5
joaoneto-hub <joaorodrigues8182@gmail.com>
2022-10-09 09:38:49 -0500

    Formulario Finalizado

--- src/components/Pages/NewProject.js
+++ src/components/Pages/NewProject.js
@@ -1,5 +1,14 @@
-function NewProject(){
-  return <h1>NewProject</h1>
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
+
+function NewProject() {
+  return (
+    <div className={styles.newProject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm btnText="Criar Projeto" />
+    </div>
+  );
 }
 
 export default NewProject;
